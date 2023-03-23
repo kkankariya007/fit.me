d4b588c8fc7029117457c11cd1b7fa4e1d253edd
Manu Sunil <46190721+manushyaaa@users.noreply.github.com>
2023-03-23 10:34:39 +0530

    test

--- opencv/api.py
+++ opencv/api.py
@@ -1,2 +1,12 @@
 import cv2
 print(cv2.__version__)
+
+''' first detect the tshirt color 
+    then type of clothing , shirt , tshirt , top etc etc'''\
+    
+img_grayscale = cv2.imread(r'.\opencv\test.jpg' , 0)
+
+cv2.imshow('grayscale image', img_grayscale)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('grayscale.jpg' , img_grayscale)
